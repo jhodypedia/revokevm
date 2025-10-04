c3ab17a0ac79cc35b90152225eda6b36ddc42ae5
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2025-10-04 17:02:49 +0700

    Update RpcEndpoint.js

--- db/models/RpcEndpoint.js
+++ db/models/RpcEndpoint.js
@@ -2,13 +2,16 @@ import { DataTypes } from "sequelize";
 import sequelize from "../index.js";
 
 const RpcEndpoint = sequelize.define("RpcEndpoint", {
-  chain_id: { type: DataTypes.INTEGER, allowNull: false },
+  chain_id: { 
+    type: DataTypes.STRING,   // ganti jadi STRING biar aman
+    allowNull: false 
+  },
   chain_name: { type: DataTypes.STRING, allowNull: false },
   rpc_url: { type: DataTypes.TEXT, allowNull: false },
   status: { type: DataTypes.ENUM("ok", "slow", "down"), defaultValue: "ok" },
   latency_ms: { type: DataTypes.INTEGER, allowNull: true },
   last_checked: { type: DataTypes.DATE, allowNull: true },
-  priority: { type: DataTypes.INTEGER, defaultValue: 100 } // makin kecil makin prioritas
+  priority: { type: DataTypes.INTEGER, defaultValue: 100 }
 }, {
   tableName: "rpc_endpoints",
   indexes: [
